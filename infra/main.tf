b8d398067edb1a6628bafa9866ca816846b6e889
nbacadlabs <nbacadlabs@nexgbits.com>
2025-06-02 17:10:19 -0400

    updated

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,9 @@
 provider "azurerm" {
   features {}
 }
+
+module "resource_group" {
+  source   = "../../../@libs/jenkins-shared-library/terraform-modules/resource-group"
+  name     = "rg-${var.environment}"
+  location = var.location
+}
