37b62fb027e5b2d12af0e7ad8690b681588db634
nbacadlabs <nbacadlabs@nexgbits.com>
2025-06-02 23:23:29 -0400

    updated

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,17 +1,15 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "rg-testval"
 }
 
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "canadacentral"
 }
 
 variable "environment" {
   description = "Azure region"
   type        = string
-  default     = "dev"
+
 }
