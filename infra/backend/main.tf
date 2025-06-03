8587395db4d769c9d534d595a1dd2b517c264005
nexgbitslabs <nexgbitslabs@nexgbits.com>
2025-06-03 19:47:31 -0400

    updated

---
+++ infra/backend/main.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source   = "../../../@libs/jenkins-shared-library/modules/resource_group"
+  name     = "rg-${var.environment}"
+  location = var.location
+}
