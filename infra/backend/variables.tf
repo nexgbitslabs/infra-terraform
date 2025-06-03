d7e4b396efb3115417a8cb48818b52668515d763
nbacadlabs <nbacadlabs@nexgbits.com>
2025-06-02 23:26:49 -0400

    updated

---
+++ infra/backend/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Azure region"
+  type        = string
+
+}
