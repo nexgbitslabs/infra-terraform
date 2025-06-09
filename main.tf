2d6ab94d5339836ebe17a8ef1f69c150879eecaf
nbacadlabs <nbacadlabs@nexgbits.com>
2025-06-09 17:24:52 -0400

    updated

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # VNET MODULE
 module "resource_group" {
-  source              = "./modules/vnet"
+  source              = "./modules/resource_group"
   name                = var.resource_group_name
   location            = var.location
 }
